4bf43b8425864c4b1c95ddfe1d37e0ac1b33320f
Alexander Slotte <alexander.slotte@gmail.com>
2019-07-21 13:29:38 -0400

    Decoupled ML.NET model to separate storage account

--- src/real-time-data-streaming/fraud-prediction-function/FraudPredictionFunction/FraudPredictionFunction/FraudPredictionFunction.cs
+++ src/real-time-data-streaming/fraud-prediction-function/FraudPredictionFunction/FraudPredictionFunction/FraudPredictionFunction.cs
@@ -2,6 +2,7 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
 using Microsoft.ML;
 using Newtonsoft.Json;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace FraudPredictionFunction
@@ -12,23 +13,24 @@ namespace FraudPredictionFunction
         [return: EventHub("transaction-enriched-eh", Connection = "eventHubConnection")]
         public static async Task<string> Run(
             [EventHubTrigger("transaction-eh", Connection = "eventHubConnection")] string messageBody,
+            [Blob("model/MLModel.zip", FileAccess.Read, Connection = "storageAccountConnection")] Stream model,
             ILogger log)
         {
             log.LogInformation("Fraud Prediction Function Called");
 
             var transaction = JsonConvert.DeserializeObject<Transaction>(messageBody);
 
-            transaction.IsFraud = IsFraudulentTransacstion(transaction);
+            transaction.IsFraud = IsFraudulentTransaction(transaction, model);
 
             var result = JsonConvert.SerializeObject(transaction);
 
             return result;
         }
 
-        private static bool IsFraudulentTransacstion(Transaction transaction)
+        private static bool IsFraudulentTransaction(Transaction transaction, Stream model)
         {
             var mlContext = new MLContext();
-            var predictionPipeline = mlContext.Model.Load(@"Data\MLModel.zip", out _);
+            var predictionPipeline = mlContext.Model.Load(model, out _);
             var predictionEngine = mlContext.Model.CreatePredictionEngine<Transaction, FraudPrediction>(predictionPipeline);
 
             return predictionEngine.Predict(transaction).IsFraud;         
