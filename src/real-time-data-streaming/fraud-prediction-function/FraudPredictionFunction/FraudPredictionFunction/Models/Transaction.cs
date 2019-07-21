3e4d0faa57eec85c13f3a5e1f3ae983f5abd607d
Alexander Slotte <alexander.slotte@gmail.com>
2019-07-21 13:46:09 -0400

    Included required packages and updated input model

--- src/real-time-data-streaming/fraud-prediction-function/FraudPredictionFunction/FraudPredictionFunction/Models/Transaction.cs
+++ src/real-time-data-streaming/fraud-prediction-function/FraudPredictionFunction/FraudPredictionFunction/Models/Transaction.cs
@@ -1,16 +1,40 @@
-﻿namespace FraudPredictionFunction
+﻿using Microsoft.ML.Data;
+
+namespace FraudPredictionFunction
 {
     public class Transaction
     {
-        public int Step { get; set; }
+        [ColumnName("step"), LoadColumn(0)]
+        public float Step { get; set; }
+
+        [ColumnName("type"), LoadColumn(1)]
         public string Type { get; set; }
-        public decimal Amount { get; set; }
+
+        [ColumnName("amount"), LoadColumn(2)]
+        public float Amount { get; set; }
+
+        [ColumnName("nameOrig"), LoadColumn(3)]
         public string NameOrig { get; set; }
-        public decimal OldBalanceOrg { get; set; }
-        public decimal NewBalanceOrig { get; set; }
+
+        [ColumnName("oldbalanceOrg"), LoadColumn(4)]
+        public float OldbalanceOrg { get; set; }
+
+        [ColumnName("newbalanceOrig"), LoadColumn(5)]
+        public float NewbalanceOrig { get; set; }
+
+        [ColumnName("nameDest"), LoadColumn(6)]
         public string NameDest { get; set; }
-        public decimal OldBalanceDest { get; set; }
-        public decimal NewBalanceDest { get; set; }
+
+        [ColumnName("oldbalanceDest"), LoadColumn(7)]
+        public float OldbalanceDest { get; set; }
+
+        [ColumnName("newbalanceDest"), LoadColumn(8)]
+        public float NewbalanceDest { get; set; }
+
+        [ColumnName("isFraud"), LoadColumn(9)]
         public bool IsFraud { get; set; }
+
+        [ColumnName("isFlaggedFraud"), LoadColumn(10)]
+        public float IsFlaggedFraud { get; set; }
     }
 }
