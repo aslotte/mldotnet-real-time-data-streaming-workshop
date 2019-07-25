bab79391d5dca7d7734ee38e9b7ca2d76858f743
Alexander Slotte <alexander.slotte@gmail.com>
2019-07-25 09:00:50 -0400

    Initial commit of training solution

---
+++ src/machine-learning/FraudPreditionTrainer/FraudPreditionTrainer/Program.cs
@@ -0,0 +1,25 @@
+﻿using FraudPreditionTrainer.Schema;
+using Microsoft.ML;
+
+namespace FraudPreditionTrainer
+{
+    class Program
+    {
+        private static string DataPath = "Data/";
+        static void Main(string[] args)
+        {
+            var mlContext = new MLContext(seed: 1);
+
+            //Load
+            var data = mlContext.Data.LoadFromTextFile<Transaction>(DataPath, hasHeader: true, separatorChar: ',');
+
+            //Train
+
+            //Transform
+
+            //Evaluate
+
+            //Save
+        }
+    }
+}
