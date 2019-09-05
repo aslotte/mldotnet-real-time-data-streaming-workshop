2e4f9a2fea473f279be428c6b6385890d4d4cd5d
Alexander Slotte <alexander.slotte@gmail.com>
2019-09-05 12:14:33 -0400

    Added VS Code optimized trainer

---
+++ src/machine-learning/FraudPredictionTrainer/Transaction.cs
@@ -0,0 +1,40 @@
+using Microsoft.ML.Data;
+
+namespace FraudPreditionTrainer
+{
+    internal sealed class Transaction
+    {
+        [ColumnName("step"), LoadColumn(0)]
+        public float Step { get; set; }
+
+        [ColumnName("type"), LoadColumn(1)]
+        public string Type { get; set; }
+
+        [ColumnName("amount"), LoadColumn(2)]
+        public float Amount { get; set; }
+
+        [ColumnName("nameOrig"), LoadColumn(3)]
+        public string NameOrig { get; set; }
+
+        [ColumnName("oldbalanceOrg"), LoadColumn(4)]
+        public float OldbalanceOrg { get; set; }
+
+        [ColumnName("newbalanceOrig"), LoadColumn(5)]
+        public float NewbalanceOrig { get; set; }
+
+        [ColumnName("nameDest"), LoadColumn(6)]
+        public string NameDest { get; set; }
+
+        [ColumnName("oldbalanceDest"), LoadColumn(7)]
+        public float OldbalanceDest { get; set; }
+
+        [ColumnName("newbalanceDest"), LoadColumn(8)]
+        public float NewbalanceDest { get; set; }
+
+        [ColumnName("isFraud"), LoadColumn(9)]
+        public bool IsFraud { get; set; }
+
+        [ColumnName("isFlaggedFraud"), LoadColumn(10)]
+        public float IsFlaggedFraud { get; set; }
+    }
+}
