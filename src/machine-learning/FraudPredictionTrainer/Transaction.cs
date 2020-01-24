2737d94dcf44ee77daa8ad4bfa68c9e2b5a2676f
Alexander Slotte <alexander.slotte@gmail.com>
2020-01-24 17:08:47 -0500

    Updated example to use nameof

--- src/machine-learning/FraudPredictionTrainer/Transaction.cs
+++ src/machine-learning/FraudPredictionTrainer/Transaction.cs
@@ -4,37 +4,37 @@ namespace FraudPredictionTrainer
 {
     public sealed class Transaction
     {
-        [ColumnName("step"), LoadColumn(0)]
+        [ColumnName("Step"), LoadColumn(0)]
         public float Step { get; set; }
 
-        [ColumnName("type"), LoadColumn(1)]
+        [ColumnName("Type"), LoadColumn(1)]
         public string Type { get; set; }
 
-        [ColumnName("amount"), LoadColumn(2)]
+        [ColumnName("Amount"), LoadColumn(2)]
         public float Amount { get; set; }
 
-        [ColumnName("nameOrig"), LoadColumn(3)]
+        [ColumnName("NameOrig"), LoadColumn(3)]
         public string NameOrig { get; set; }
 
-        [ColumnName("oldbalanceOrg"), LoadColumn(4)]
+        [ColumnName("OldbalanceOrg"), LoadColumn(4)]
         public float OldbalanceOrg { get; set; }
 
-        [ColumnName("newbalanceOrig"), LoadColumn(5)]
+        [ColumnName("NewbalanceOrig"), LoadColumn(5)]
         public float NewbalanceOrig { get; set; }
 
-        [ColumnName("nameDest"), LoadColumn(6)]
+        [ColumnName("NameDest"), LoadColumn(6)]
         public string NameDest { get; set; }
 
-        [ColumnName("oldbalanceDest"), LoadColumn(7)]
+        [ColumnName("OldbalanceDest"), LoadColumn(7)]
         public float OldbalanceDest { get; set; }
 
-        [ColumnName("newbalanceDest"), LoadColumn(8)]
+        [ColumnName("NewbalanceDest"), LoadColumn(8)]
         public float NewbalanceDest { get; set; }
 
-        [ColumnName("isFraud"), LoadColumn(9)]
+        [ColumnName("IsFraud"), LoadColumn(9)]
         public bool IsFraud { get; set; }
 
-        [ColumnName("isFlaggedFraud"), LoadColumn(10)]
+        [ColumnName("IsFlaggedFraud"), LoadColumn(10)]
         public float IsFlaggedFraud { get; set; }
     }
 }
